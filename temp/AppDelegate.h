86dee6238f6adeb57607b8ca22827bbe65c3e695
mellwa <kolachao.chen@gmail.com>
2014-02-22 18:36:22 -0500

    Initial Commit

---
+++ temp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by Chao Chen on 2/22/2014.
+//  Copyright (c) 2014 Chao Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ temp/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  temp
+//
+
+//  Copyright (c) 2014 Chao Chen. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
